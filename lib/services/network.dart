06b3c7aa9b53fc99dab49b2fb5e9decf86d51c10
Tim Kravel <TKravel@Yahoo.com>
2024-07-01 23:11:35 -0400

    Update: carousel similar drinks search func - comments

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -4,20 +4,35 @@ import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+/// Functionality for making http requests to outside sources
 class Network {
+  /// API base url
   final String _authority = 'www.thecocktaildb.com';
+
+  /// API key for extended functionality
   final String _apiKey = dotenv.env['API_KEY']!;
 
+  /// Make requests to the cocktaildb API
+  ///
+  /// [url] The API url to hit
+  /// [params] Optional packaged API params
+  ///
+  /// Returns response.body
   apiPostRequest(String url, Map<String, String>? params) async {
     String routeBuilder = 'api/json/$_apiKey$url';
     Uri endPoint = Uri.https(_authority, routeBuilder, params);
-    log(endPoint.toString());
     var response = await http.get(endPoint);
     // log('Response status: $url ${response.statusCode}');
     // log('Response body: ${response.body}');
     return response.body;
   }
 
+  /// Make http requests
+  ///
+  /// [url] The url to hit
+  /// [bodyPkg] Request body
+  ///
+  /// Returns response.body
   postRequest(Uri url, Map<String, dynamic> bodyPkg) async {
     var response = await http.post(url, body: bodyPkg);
     return response;
--- lib/widgets/carousel/carousel.dart
+++ lib/widgets/carousel/carousel.dart
@@ -9,7 +9,9 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:mix_it/services/network.dart';
 import 'package:mix_it/models/drink_model.dart';
 import 'package:mix_it/utils/colors/custom_colors.dart';
+import 'package:mix_it/utils/constants/constants.dart';
 
+/// Based off of selected drink will show similar drinks in a carousel
 class Carousel extends StatefulWidget {
   const Carousel(
       {super.key,
@@ -17,8 +19,13 @@ class Carousel extends StatefulWidget {
       required this.drinkCategory,
       required this.ingredients});
 
+  /// Current ID for selected drink
   final String drinkId;
+
+  /// Category of selected drink
   final String drinkCategory;
+
+  /// Ingredient measurements keyed by ingredient name
   final Map<String, String> ingredients;
 
   @override
@@ -28,50 +35,76 @@ class Carousel extends StatefulWidget {
 }
 
 class _CarouselState extends State<Carousel> {
-  var isLoading = true;
-  List<Drink> similarDrinks = [];
-  int ingredientsCount = 0;
-  int currentIdx = 1;
+  /// Whether widget is loading
+  bool isLoading = true;
+
+  /// Whether similar drinks were found
   bool noSimilarDrinks = false;
+
+  /// Total ingredients for selected drink
+  int ingredientsCount = 0;
+
+  /// Similar drinks
+  List<Drink> similarDrinks = [];
+
+  /// Carousel controller
   CarouselController buttonCarouselController = CarouselController();
 
   @override
   void initState() {
     super.initState();
     ingredientsCount = widget.ingredients.keys.length;
-    _getSimilarDrinks(_getIngredientString());
+    getSimilarDrinks();
   }
 
-  _getSimilarDrinks(ingredient) async {
-    if (currentIdx > ingredientsCount) {
-      noSimilarDrinks = true;
-      isLoading = false;
-    }
-    const String similiarRoute = "/filter.php";
-    Map<String, String> params2 = {'i': ingredient};
+  /// Get similar drinks based on ingredients.
+  /// Searches for similar drinks one ingredient at at time until ingredient list is exhausted.
+  /// Updates [isLoading] and [noSimilarDrinks] state variables based on results.
+  getSimilarDrinks() async {
+    int currentIdx = 0;
     Network networkHandler = Network();
-    String similarResponse =
-        await networkHandler.apiPostRequest(similiarRoute, params2);
-    var similarJson = jsonDecode(similarResponse);
-    if (similarJson['drinks'].length == 0) {
+    dynamic similarJson;
+    // Call API for similar drinks
+    do {
+      // Break if list is exhausted - set json variable to null
+      if (currentIdx == ingredientsCount) {
+        similarJson = null;
+        break;
+      }
+      // Call API with ingredient based on current iteration
+      Map<String, String> params = {'i': _getIngredientString(currentIdx)};
+      String similarResponse =
+          await networkHandler.apiPostRequest("/filter.php", params);
+      similarJson = jsonDecode(similarResponse);
       currentIdx++;
-      _getSimilarDrinks(_getIngredientString());
-    }
-    similarDrinks = [];
-    for (var i = 0; i < similarJson['drinks'].length; i++) {
-      if (similarJson['drinks'][i]["idDrink"] != widget.drinkId) {
-        similarDrinks.add(Drink.fromJson(similarJson['drinks'][i]));
+      // Continue loop while results are less than 2 (excludes current drink)
+    } while (similarJson['drinks'].length < 2);
+
+    // Json exists with at least 2 drinks
+    // Populate drink list with drinks from Json - removing selected drink
+    if (similarJson != null) {
+      for (var i = 0; i < similarJson['drinks'].length; i++) {
+        if (similarJson['drinks'][i]["idDrink"] != widget.drinkId) {
+          similarDrinks.add(Drink.fromJson(similarJson['drinks'][i]));
+        }
       }
     }
+
+    // Update state for rebuild
     setState(() {
       isLoading = false;
+      noSimilarDrinks = similarDrinks.isEmpty ? true : false;
     });
   }
 
-  _getIngredientString() {
+  /// Sanitize ingredient name for API call
+  ///
+  /// Returns ingredient by index
+  _getIngredientString(int currentIdx) {
     return widget.ingredients.keys.elementAt(currentIdx).replaceAll(" ", "_");
   }
 
+  /// Selection of similar drink functionality
   _handleTap(String drinkId) {
     Navigator.push(
       context,
@@ -86,13 +119,16 @@ class _CarouselState extends State<Carousel> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        if (isLoading == true)
-          const Text("loading...")
-        else if (noSimilarDrinks)
-          const Text("No similar drinks...")
-        else
+    // Return loading message
+    if (isLoading == true) {
+      return const CarouselMessageContainer(message: "Loading...");
+      // Return no results message
+    } else if (noSimilarDrinks) {
+      return const CarouselMessageContainer(message: "No similar drinks");
+      // Return Carousel of similar drinks
+    } else {
+      return Stack(
+        children: [
           CarouselSlider.builder(
             itemCount: similarDrinks.length,
             carouselController: buttonCarouselController,
@@ -138,47 +174,68 @@ class _CarouselState extends State<Carousel> {
               ),
             ),
           ),
-        Positioned(
-          left: 0,
-          top: 115,
-          child: Ink(
-            child: IconButton(
-              color: CustomColors.kAccent.shade100,
-              iconSize: 40,
-              onPressed: () => buttonCarouselController.previousPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.linear),
-              icon: const Icon(
-                Icons.chevron_left_rounded,
+          Positioned(
+            left: 0,
+            top: 115,
+            child: Ink(
+              child: IconButton(
+                color: CustomColors.kAccent.shade100,
+                iconSize: 40,
+                onPressed: () => buttonCarouselController.previousPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.linear),
+                icon: const Icon(
+                  Icons.chevron_left_rounded,
+                ),
               ),
             ),
           ),
-        ),
-        Positioned(
-          right: 0,
-          top: 115,
-          child: IconButton(
-            color: CustomColors.kAccent.shade100,
-            iconSize: 40,
-            onPressed: () => buttonCarouselController.nextPage(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.linear),
-            icon: const Icon(
-              Icons.chevron_right_rounded,
+          Positioned(
+            right: 0,
+            top: 115,
+            child: IconButton(
+              color: CustomColors.kAccent.shade100,
+              iconSize: 40,
+              onPressed: () => buttonCarouselController.nextPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.linear),
+              icon: const Icon(
+                Icons.chevron_right_rounded,
+              ),
             ),
           ),
-        ),
-        Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: Text(
-            style: Theme.of(context).textTheme.titleLarge,
-            textAlign: TextAlign.center,
-            "Similar Drinks (${similarDrinks.length})",
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Text(
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
+              "Similar Drinks (${similarDrinks.length})",
+            ),
           ),
-        ),
-      ],
+        ],
+      );
+    }
+  }
+}
+
+/// Reusable message container
+class CarouselMessageContainer extends StatelessWidget {
+  const CarouselMessageContainer({super.key, required this.message});
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 50),
+      decoration: BoxDecoration(color: kScaffoldContainer),
+      child: Text(
+        style: Theme.of(context).textTheme.titleLarge,
+        message,
+      ),
     );
   }
 }
